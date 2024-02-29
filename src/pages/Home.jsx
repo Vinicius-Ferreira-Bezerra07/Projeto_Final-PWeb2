74b2255259b59ef09b6ba4599da7b05e236dbaef
Vinicius-Ferreira-Bezerra07 <vinicius.ferreira.bezerra07@aluno.ifce.edu.br>
2024-02-29 13:11:56 -0300

    Backup: Alteração da pagina Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
 export default function Home(){
     return (
         <div className="Home">
-            <h1>Teste</h1>
+            <h1>Projeto Final PWeb2.</h1>
+            <p>Equipe: Vinicius Ferreira Bezerra</p>
+            <p>Jhonatas Willy Lourenço</p>
+            <p>API: "https://apidocs.cheapshark.com"</p>
         </div>
     )
 }
